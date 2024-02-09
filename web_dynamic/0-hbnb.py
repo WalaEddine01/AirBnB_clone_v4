80e9d38a960f9b88e162b40dde56eea7bb61416f
walaeddine01 <walaaeddine33@gmail.com>
2024-02-09 11:43:09 +0100

    I made the changes on 2024-02-09 11:43:09

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from models import storage
+from uuid import uuid4
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
@@ -18,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,10 +35,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
