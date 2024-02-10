8bbc185c35d29c60019d066992a77558c3355bce
walaeddine01 <walaaeddine33@gmail.com>
2024-02-10 13:55:49 +0100

    I made the changes on 2024-02-10 13:55:49

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $.get('http://localhost:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
