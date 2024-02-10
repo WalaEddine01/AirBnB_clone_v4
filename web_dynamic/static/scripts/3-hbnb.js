be0bf89db5e4b377779209fe980dbb0f5b2d0cd2
walaeddine01 <walaaeddine33@gmail.com>
2024-02-10 15:33:18 +0100

    I made the changes on 2024-02-10 15:33:18

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,43 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   const url = "http://localhost:5001/api/v1/places_search";
-  $.ajax(
-    {
-      url: url,
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({}),
-      success: function (data) {
-        for (const place of data) {
-          const article = $('<article></article>');
-          const title = $('<div class="title"></div>');
-          title.append('<h2>' + place.name + '</h2>');
-          title.append('<div class="price_by_night">$' + place.price_by_night + '</div>');
-          article.append(title);
-          const information = $('<div class="information"></div>');
-          const maxGuest = place.max_guest + ' Guest';
-          if (place.max_guest > 1) {
-            maxGuest += 's';
-          }
-          const numberRooms = place.number_rooms + ' Bedroom';
-          if (place.number_rooms > 1) {
-            numberRooms += 's';
-          }
-          const numberBathrooms = place.number_bathrooms + ' Bathroom';
-          if (place.number_bathrooms > 1) {
-            numberBathrooms += 's';
-          }
-          information.append('<div class="max_guest">' + maxGuest + '</div>');
-          information.append('<div class="number_rooms">' + numberRooms + '</div>');
-          information.append('<div class="number_bathrooms">' + numberBathrooms + '</div>');
-          article.append(information);
-          const description = $('<div class="description"></div>');
-          description.append(place.description);
-          article.append(description);
-          $('section.places').append(article);
-        }
-      }
+  $.ajax({
+    type: "POST",
+    url: url,
+    contentType: "application/json",
+    data: JSON.stringify({}),
+  }).done(function (data) {
+    for (const place of data) {
+      const article = ['<article>',
+        '<div class="title_box">',
+        `<h2>${place.name}</h2>`,
+        `<div class="price_by_night">$${place.price_by_night}</div>`,
+        '</div>',
+        '<div class="information">',
+        `<div class="max_guest">${place.max_guest} Guest(s)</div>`,
+        `<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`,
+        `<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>`,
+        '</div>',
+        '<div class="description">',
+        `${place.description}`,
+        '</div>',
+        '</article>'];
+      $('.places').append(article.join(''));
     }
-  );
+  });
 });
 document.addEventListener('DOMContentLoaded', function () {
   $.get('http://localhost:5001/api/v1/status/', function (data, status) {
