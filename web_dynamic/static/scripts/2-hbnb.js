45f5ab82cd3114fa9e52d8c846609a047fef1fa5
walaeddine01 <walaaeddine33@gmail.com>
2024-02-10 11:42:21 +0100

    I made the changes on 2024-02-10 11:42:21

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const res = $.get('http://0.0.0.0:5001/api/v1/status/');
-  if (res.status === 'OK') {
-    $('#api_status').addClass('available');
-  }
-  else {
-    $('#api_status').removeClass('available');
-  }
+  $.get('http://localhost:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
